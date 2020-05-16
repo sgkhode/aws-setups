7b36f0de26966ab5f20f59e53155931bc0de1a38
Sandeep G. Khode <sgkhode@gmail.com>
2020-05-16 14:15:00 +0530

    Update setup-nodejs-production-server-on-ubuntu-1804.sh

--- setup-nodejs-production-server-on-ubuntu-1804.sh
+++ setup-nodejs-production-server-on-ubuntu-1804.sh
@@ -20,22 +20,22 @@ echo "
 "
 
 # import mongodb 4.0 public gpg key
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
 
 # create the /etc/apt/sources.list.d/mongodb-org-4.0.list file for mongodb
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+#echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu bionic/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
 
 # reload local package database
-sudo apt-get update
+#sudo apt-get update
 
 # install the latest version of mongodb
-sudo apt-get install -y mongodb-org
+#sudo apt-get install -y mongodb-org
 
 # start mongodb
-sudo systemctl start mongod
+#sudo systemctl start mongod
 
 # set mongodb to start automatically on system startup
-sudo systemctl enable mongod
+#sudo systemctl enable mongod
 
 
 echo "
